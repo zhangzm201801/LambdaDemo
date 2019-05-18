07d8384b89ce3ec021cd7a8ad2568464d56dcfa7
zhangzm201801 <zhangzm1117@126.com>
2019-05-18 17:58:56 +0900

    @FunctionalInterface

--- src/Calc.java
+++ src/Calc.java
@@ -1,4 +1,15 @@
 
+/*
+ * Java中使用Lambda表达式的前提是：必须有函数式接口
+ * 
+ * 函数式接口：有且仅有一个抽象方法的接口
+ * 
+ * 如何才能万无一失的加测一下当前接口是函数式接口？
+ * 用一个固定的格式写在 interface定义之前一行即可
+ * @FunctionalInterface
+ * 
+ */
+@FunctionalInterface
 public interface Calc {
 	int sum(int a ,int b);
 }
