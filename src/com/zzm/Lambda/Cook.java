d0569d167e534ac74328a77e0b8a97f5957735e4
zhangzm201801 <zhangzm1117@126.com>
2019-05-18 21:58:12 +0900

    Lambda存在的问题

---
+++ src/com/zzm/Lambda/Cook.java
@@ -0,0 +1,8 @@
+package com.zzm.Lambda;
+
+public class Cook {
+	
+	public static void makeFood(String food) {
+		System.out.println("将" + food + "做成可口的食物");
+	}
+}
---
+++ src/com/zzm/Lambda/SitterLambda.java
@@ -0,0 +1,21 @@
+package com.zzm.Lambda;
+
+/*
+ * 在某些场景中，Lambda表达式要做的事情，其实在另外一个地方已经谢过了
+ * 那么此时通过Lambda表达式重复编写相同的代码，就是一种浪费
+ * 那么如何复用已经存在的方法逻辑呢？
+ * 
+ * 
+ */
+public class SitterLambda {
+	public static void main(String[] args) {
+		hireSitter(food -> System.out.println("将" + food + "做成可口的食物"));
+		
+		System.out.println("------------------");
+		Cook.makeFood("白菜");
+	}
+	
+	public static  void hireSitter(Sitter sitter) {
+		sitter.work("白菜");
+	}
+}
